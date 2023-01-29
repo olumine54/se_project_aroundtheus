943f330a2036e4e3421e9a4bf8f6735f97291d0e
Olumide Falade <olumine54@gmail.com>
2023-01-29 04:40:12 -0500

    JavaS

---
+++ scripts/index.js
@@ -0,0 +1,28 @@
+let initialCards = [
+    yosimteCard = {
+        name: "Yosimite Valley",
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
+    },
+    lakeLouiseCard = {
+        name: "Lake Louise",
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
+    },
+    baldMountainCard = {
+        name: "Bald Mountains",
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
+    },
+    latemarCard = {
+        name: "Latemar",
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
+    
+    },
+    vanoiseParkCard = {
+        name: "Vanoise National Park",
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
+    },
+    lagoCard = {
+        name: "Lago fi Braies",
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
+    },
+]
+    
