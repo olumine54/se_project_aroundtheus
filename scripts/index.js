3b82d6a1d8c694b5c21f9f440fcdb5cf828abcb5
Olumide Falade <olumine54@gmail.com>
2023-02-14 22:59:58 -0500

    index.jss & css adjustment

--- scripts/index.js
+++ scripts/index.js
@@ -37,9 +37,23 @@ const profileDescription = document.querySelector(".profile__description");
 const profileTitleInput = document.querySelector("#profile-title-input");
 const profileDescriptionInput = document.querySelector(
     "#profile-description-input");
+    // preview moda
+const previewModal = document.querySelector("#preview-image-modal");
+const previewImage = document.querySelector(".modal__preview-image");
+const previewCloseModal = previewModal.querySelector("#preview-close-button");
+const previewFooter = document.querySelector(".modal__preview-footer");
+
 const profileEditForm = profileEditModal.querySelector(".modal__form");
+
 const cardListEl = document.querySelector(".cards__list");
 const cardTemplate = document.querySelector("#card-template").content.firstElementChild;
+const addCardButton = document.querySelector(".profile__add-button");
+const cardEditModal = document.querySelector("#card-edit-modal");
+const addCardForm = cardEditModal.querySelector(".modal__form");
+const cardModalCloseButton = cardEditModal.querySelector(".modal__close-button");
+const cardTitleInput = addCardForm.querySelector("#card-title-input");
+const cardUrlInput = addCardForm.querySelector("#card-url-input");
+
 
 function closePopup(modal) {
     modal.classList.remove("modal_opened");
@@ -51,6 +65,27 @@ function getCardElement(cardData) {
     // access the card title and image and store them in variables
     const cardImageEl = cardElement.querySelector(".card__image");
     const cardTitleEl = cardElement.querySelector(".card__title");
+    //add delete button
+    const deleteButton = cardElement.querySelector(".card__delete-button");
+    //add event listener
+     deleteButton.addEventListener("click", () => {
+     deleteButton.classList.toggle("card__delete-button");
+     cardElement.remove();
+    });
+    // add clicklistener to the cardImage element
+     cardImageEl.addEventListener("click", () => {
+        previewImage.src = cardData.link;
+        previewImage.alt = cardData.name;
+        previewFooter.textContent = cardData.name;
+      previewModal.classList.toggle("modal_opened");
+    });
+    //open modal with previewimage modal
+    // set card like button here
+    const likeButton = cardElement.querySelector(".card__like-button");
+       likeButton.addEventListener("click", () => {
+       likeButton.classList.toggle("card__like-button_active");
+       });
+       
     // set the path to the image to the link field of the object
     cardImageEl.src = cardData.link;
     // set the image alt text to the name field of the object
@@ -76,10 +111,43 @@ function getCardElement(cardData) {
     profileTitle.textContent = profileTitleInput.value;
     profileDescription.textContent = profileDescriptionInput.value;
     closePopup(profileEditModal);
+ });
+
+ previewCloseModal.addEventListener("click", (e)=>{
+    closePopup(previewModal);
+ });
+
+initialCards.forEach((cardData) => renderCard(cardData, cardListEl))
+
+// add new card button
+function renderCard(cardData, cardListEl) {
+    const cardElement = getCardElement(cardData)
+    cardListEl.append(cardElement);
+}
+
+addCardButton.addEventListener("click", () => {
+    // cardTitleInput.value = cardTitle.textContext;
+    // cardUrlInput.value = cardDescription.textContent;
+    cardEditModal.classList.add("modal_opened");
+});
+
+ addCardForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+   const name = cardTitleInput.value;
+  const link = cardUrlInput.value;
+   const cardElement = getCardElement
+   renderCard({ name, link }, cardListEl);
+    closePopup(cardEditModal);
  })
 
-initialCards.forEach((cardData) => {
-    const cardElement = getCardElement(cardData);
-    cardListEl.append(cardElement);
-})
 
+cardModalCloseButton.addEventListener("click", () => {
+    closePopup(cardEditModal);
+ });
+
+//  const likeButtons = document.querySelectorAll(".card__like-button");
+//  likeButtons.forEach((likeButton) =>{
+//     likeButton.addEventListener("click", () =>{
+//     likeButton.classList.toggle("card__like-button_active");
+//     })
+//  })
